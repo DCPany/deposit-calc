be6bb26e90551520745230142a6fd70ba7880381
DCP <mmtynky@gmail.com>
2017-06-01 20:42:13 +0700

    Change error function

--- main.c
+++ main.c
@@ -6,15 +6,15 @@ int main()
 {
 	int summa,srok;
 	printf("enter sum of deposit \n");
-	scanf("%i",summa);
+	scanf("%i",&summa);
 	printf("enter time of deposit \n");
-	scanf("%i",srok);
+	scanf("%i",&srok);
 	if ((summa<10000) || (srok>365) || (srok<=0))
 	printf("enter correct data! \n");
     else
 	{
 		printf("sum at the end of: ");
-        printf("%i",proffit(summa,srok));
+        printf("%i \n",proffit(summa,srok));
     }
     return 0;
 }
