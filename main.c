cd7db987d7097fd544ad52e1afca64cd7819842c
DCP <mmtynky@gmail.com>
2017-06-01 20:08:30 +0700

    Create function main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int summa,srok;
+	printf("Сумма \n");
+	scanf("Сумма %f \n",summa);
+	printf("Срок \n");
+	scanf("Срок %f \n",srok);
+	if (summa>=1000) && (srok<365);
+}
